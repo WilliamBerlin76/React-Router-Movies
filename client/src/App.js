d596d87463e8ca2ecfa602534e80c68766b53d0a
WilliamBerlin76 <william.berlin76@gmail.com>
2019-09-16 16:38:25 -0500

    part 1 complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import {BrowserRouter as Router, Route } from "react-router-dom";
 import SavedList from './Movies/SavedList';
 
 const App = () => {
@@ -13,6 +13,8 @@ const App = () => {
     <div>
       <SavedList list={savedList} />
       <div>Replace this Div with your Routes</div>
+      <Route exact path="/" component={MovieList} />
+      <Route exact path="/movies/:id" component={Movie} />
     </div>
   );
 };
