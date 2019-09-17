58cb4519c7f97a38cfc02d4cba646783549abf18
WilliamBerlin76 <william.berlin76@gmail.com>
2019-09-17 14:57:35 -0500

    part one actually complete, got everything displayed and made the /:id route dynamic

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import {BrowserRouter as Router, Route } from "react-router-dom";
 import SavedList from './Movies/SavedList';
-
+import Movie from './Movies/Movie';
+import MovieList from './Movies/MovieList';
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
 
@@ -12,9 +13,9 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
       <Route exact path="/" component={MovieList} />
-      <Route exact path="/movies/:id" component={Movie} />
+      <Route exact path={`/movies/${Movie}`} component={Movie} />
+      
     </div>
   );
 };
